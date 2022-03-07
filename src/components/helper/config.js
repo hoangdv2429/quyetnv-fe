d3ef7125e410a01fe1416753b30955f46a88fe25
Hieu Vu <hieuvubk@gmail.com>
2022-03-08 02:59:53 +0700

    upload excel => save pdf: pdf border, scale still err but data ok

--- src/components/helper/config.js
+++ src/components/helper/config.js
@@ -8,7 +8,7 @@ export const axiosInstance = axios.create({
   responseType: 'json',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imh1c3QyMDIyQGdtYWlsLmNvbSIsImlzc3VlcklkIjoiNjFlNTNmOWY1ZjNjMGRmM2QzOWVmNWU1Iiwicm9sZXMiOlsxXSwiaWF0IjoxNjQ2NjQ4OTA4LCJleHAiOjE2NDY2NTI1MDh9.FeKSues1yfRM0_WuL1LsLDH3T-0cnvkb-V-2MvduKiI`,
+    'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imh1c3QyMDIyQGdtYWlsLmNvbSIsImlzc3VlcklkIjoiNjFlNTNmOWY1ZjNjMGRmM2QzOWVmNWU1Iiwicm9sZXMiOlsxXSwiaWF0IjoxNjQ2Njc5Njk3LCJleHAiOjE2NDY2ODMyOTd9.Sn-SxqonceB7dKwRVLOajo-54RbSKCc8aWpcqOPLTQw`,
   },
 }); 
 

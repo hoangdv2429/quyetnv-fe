c3b6e3d3f0b658abe833fdab8bff8aa3eb0f5c3c
Hieu Vu <hieuvubk@gmail.com>
2022-03-10 11:14:47 +0700

    upload excel

--- src/components/helper/config.js
+++ src/components/helper/config.js
@@ -8,7 +8,7 @@ export const axiosInstance = axios.create({
   responseType: 'json',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imh1c3QyMDIyQGdtYWlsLmNvbSIsImlzc3VlcklkIjoiNjFlNTNmOWY1ZjNjMGRmM2QzOWVmNWU1Iiwicm9sZXMiOlsxXSwiaWF0IjoxNjQ2Njc5Njk3LCJleHAiOjE2NDY2ODMyOTd9.Sn-SxqonceB7dKwRVLOajo-54RbSKCc8aWpcqOPLTQw`,
+    'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Imh1c3QyMDIyQGdtYWlsLmNvbSIsImlzc3VlcklkIjoiNjFlNTNmOWY1ZjNjMGRmM2QzOWVmNWU1Iiwicm9sZXMiOlsxXSwiaWF0IjoxNjQ2NzEwNDg3LCJleHAiOjE2NDY3MTQwODd9.J106d7t5_FcE2jbOPnAiM3s6lFT41pjeixSFQIJYfPA`,
   },
 }); 
 

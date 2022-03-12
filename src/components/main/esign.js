9c8495aea4d661b518f564d4638de0f9939e8f41
Đỗ Việt Hoàng <hoangdv2429@gmail.com>
2022-03-12 11:28:38 +0700

    adding qr code

--- src/components/main/esign.js
+++ src/components/main/esign.js
@@ -97,6 +97,26 @@ const Esign = () => {
       console.log ( updatedJson );
   }
 
+//   import QrCode from 'react-qrcode-svg';
+
+// const Qrcodetosvg = (data) => {
+
+//         return (
+//             <div>
+//                 <QrCode
+//         data="google.com"
+//         height="250"
+//         width="250"
+//         fgColor="#000000"
+//         bgColor="#ffffff"
+//         />
+//             </div>
+        
+//         );
+// }
+
+// export default Qrcodetosvg;
+
   return (
     <>
       <Header />
